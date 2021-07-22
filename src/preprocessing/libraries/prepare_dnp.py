5352875efd922d1b6abe1737275ecb6ae8e70eb2
Adafede <adriano.rutz@unige.ch>
2021-07-23 00:36:16 +0200

    update

--- src/preprocessing/libraries/prepare_dnp.py
+++ src/preprocessing/libraries/prepare_dnp.py
@@ -1,2 +1,62 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""This script prepares DNP structure-organism pairs \n,
+   for further processing"""
+
+import os
+
+import pandas
+import yaml
+
+with open("paths.yaml", 'r') as stream:
+    try:
+        paths = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+os.chdir(paths["base_dir"])
+
+with open("config/default/prepare_dnp.yaml", 'r') as stream:
+    try:
+        params = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+with open("config/params/prepare_dnp.yaml", 'r') as stream:
+    try:
+        params = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+if os.path.isfile(params["input"]):
+    file_initial = pandas.read_csv(params["input"])
+
+    file_formatted = \
+        file_initial.assign(structure_inchikey_2D=file_initial.structure_inchikey.str[0:13], reference_doi="")[[
+            # 'structure_name',
+            'structure_inchikey_2D',
+            'structure_smiles_2D',
+            'structure_molecular_formula',
+            'structure_exact_mass',
+            # 'structure_xlogp',
+            'structure_taxonomy_npclassifier_01pathway',
+            'structure_taxonomy_npclassifier_02superclass',
+            'structure_taxonomy_npclassifier_03class',
+            'organism_name', 'organism_taxonomy_01domain',
+            'organism_taxonomy_02kingdom',
+            'organism_taxonomy_03phylum',
+            'organism_taxonomy_04class',
+            'organism_taxonomy_05order',
+            'organism_taxonomy_06family',
+            'organism_taxonomy_07tribe',
+            'organism_taxonomy_08genus',
+            'organism_taxonomy_09species',
+            'organism_taxonomy_10varietas',
+            # 'reference_title',
+            'reference_doi']].drop_duplicates()
+
+    file_formatted.to_csv(params["output"])
+
+else:
+    print("""Sorry, you do not have access to the DNP""")
