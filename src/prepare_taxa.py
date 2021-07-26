802edb13ffedfd67184e31ff63acd18068df229e
Adafede <adriano.rutz@unige.ch>
2021-07-26 07:53:41 +0200

    update

--- src/prepare_taxa.py
+++ src/prepare_taxa.py
@@ -1,2 +1,68 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+"""This script prepares taxa"""
+
+from sys import exit
+
+import pandas
+
+from helpers.get_gnps import read_features
+from helpers.get_gnps import read_metadata
+from helpers.parse_yaml_params import parse_yaml_params
+from helpers.parse_yaml_paths import parse_yaml_paths
+
+paths = parse_yaml_paths()
+
+params = parse_yaml_params(step="prepare_taxa")
+
+if params["tool"] in ['gnps', 'manual']:
+    print(""" Tool parameter OK""")
+else:
+    print(
+        """Your --tool.metadata parameter (in command line arguments or in 'inform_params.yaml' must be either 'gnps' or 'manual'""")
+    exit()
+
+if params["top_k"] <= 5:
+    print(""" Top K parameter OK""")
+else:
+    print(
+        """Your --top_k.organism_per_feature parameter (in command line arguments or in 'inform_params.yaml' should be lower or equal to 5""")
+    exit()
+
+taxa_ranks_dictionary = pandas.read_csv(
+    filepath_or_buffer=paths["data"]["source"]["dictionaries"]["ranks"]
+)
+
+## TODO manual possibility to add
+
+if params["tool"] == 'gnps':
+    feature_table = read_features(gnps=params["gnps"]).filter(
+    	regex='(row ID)|( Peak area)'
+    	)
+    metadata_table = read_metadata(gnps=params["gnps"])
+
+    feature_table.columns = feature_table.columns.str.rstrip(' Peak area')
+
+    feature_table = pandas.melt(
+    	feature_table,
+    	id_vars=['row ID']
+    	)
+
+    feature_table = feature_table[feature_table['value'] != 0]
+
+    feature_table["rank"] = feature_table.groupby(
+    	"row ID")["value"].rank(
+    	"dense", ascending=False)
+
+    feature_table = feature_table[feature_table['rank'] <= params["top_k"]]
+
+    ## rest to come ...
+    
+    print(metadata_table)
+    print(feature_table)
+
+else:
+    print("""manual version still to do, Sorry""")
+
+## TODO export params when modified with CLI
