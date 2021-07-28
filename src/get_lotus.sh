2bf829293c88a2bd70f00c920ceb1dd89bc96e40
Jonathan Bisson <bjo@uic.edu>
2021-07-28 11:23:36 -0500

    [quality] run everything from path
    Running from src was stupid.

--- src/get_lotus.sh
+++ src/get_lotus.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+source "$SCRIPT_DIR"/parse_yaml.sh
+source "$SCRIPT_DIR"/warning.sh
 
-source parse_yaml.sh
-source warning.sh
-
-eval $(parse_yaml paths.yaml)
-
-cd $base_dir
+eval $(parse_yaml "$SCRIPT_DIR"/paths.yaml)
 
 mkdir -p $data_source_libraries_path
 
---
+++ src/warning.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+
+if [ ! -d config ]; then
+  echo "Sorry, you need to run that from where your config is."
+  exit 1
+fi
