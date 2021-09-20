37b8f5e6874145caf1ae5ec53bd9b48e03991e5b
Adriano Rutz <adriano.rutz@hotmail.com>
2021-09-20 22:20:08 +0000

    Update get_gnverifier.sh

--- src/get_gnverifier.sh
+++ src/get_gnverifier.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-source "$SCRIPT_DIR"/get_platform.sh
-source "$SCRIPT_DIR"/parse_yaml.sh
-source "$SCRIPT_DIR"/warning.sh
+source src/get_platform.sh
+source src/parse_yaml.sh
+source src/warning.sh
 
 eval $(parse_yaml paths.yaml)
-cd $base_dir || exit
 eval $(parse_yaml config/versions.yaml)
 
 mkdir -p $bin_path
-wget -O - https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-$OS.tar.gz | tar xOz gnverifier >bin/gnverifier
+wget "https://github.com/gnames/gnverifier/releases/download/$gnverifier/gnverifier-$gnverifier-$OS.tar.gz" -P bin/
+tar -xzf bin/gnverifier-$gnverifier-$OS.tar.gz -C bin/
+rm -r bin/gnverifier-$gnverifier-$OS.tar.gz
 chmod +x bin/gnverifier
